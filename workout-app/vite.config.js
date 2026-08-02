3d693f863f4a7d2f46bd9f8ad3e0eeafed54c258
Roomote <roomote@roomote.dev>
2026-08-02 19:32:36 +0000

    Configure GitHub Pages deployment

--- workout-app/vite.config.js
+++ workout-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fitplan/', // Замените 'fitplan' на имя вашего репозитория
+  base: '/app-workout/', // Имя репозитория на GitHub
 })
